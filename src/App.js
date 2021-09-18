9a70169561fd23d643458f4e2dd54e695467ce2f
bbossio <bibianabossio@gmail.com>
2021-09-17 21:02:02 -0300

    17.9 root

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Contrasenia from './pages/Login/components/Contrasenia/Contrasenia';
 function App() {
   return (
     <div className="App">
-     <Contrasenia/>
+     <Login/>
     </div>
   );
 }
