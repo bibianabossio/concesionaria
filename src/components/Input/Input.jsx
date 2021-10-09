9f0844b12400a8a3a8ce0b6d333b970ec276590a
bbossio <bibianabossio@gmail.com>
2021-10-08 23:08:40 -0300

    8.10 colores letras

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -4,14 +4,15 @@ import '../Login/Login.css';
 const Input = ({attribute, handleChange, param}) => {
     return (
    
-        <div className='input.container'>
-         <input
+        <div className='input.container'  >
+         <input 
             id={attribute.id}
             name={attribute.name}
             placeholder={attribute.placeholder}
             type={attribute.type}
             onChange={ (e) => handleChange(e.target.name, e.target.value)}
-            className={ param ? 'input-error':'regular-style'}
+            className={ param ? 'input-error':'regular-style' }
+           
             />
         </div>
     )
