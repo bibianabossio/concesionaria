b15e6f9b3c97b435ab39803dfe5cb888e8596e75
cataldoivan <cmiarg@gmail.com>
2021-10-16 18:29:02 -0300

    agrego posibilidad dee editar automoviles

--- src/components/ListadoAutomoviles/ListadoAutomoviles.js
+++ src/components/ListadoAutomoviles/ListadoAutomoviles.js
@@ -1,34 +1,31 @@
 import { AvTimer } from "@mui/icons-material";
-import  { Component } from "react";
-import { BrowserRouter as Router,  Link } from "react-router-dom";
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import { Component,useContext,useState,useEffect } from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 import Loading from "../Loading/Loading";
+import BarraNavegacionContexto from "../../context/BarraNavegacionContexto";
 
+const  Auto =()=> {
+  const [objeto, setObjeto] = useState([])
+  const { handleSeleccion, handleSubmitModificar } = useContext(BarraNavegacionContexto);
+ 
 
-export default class Auto extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { objeto: [] };
-   
+  useEffect(async() => {
     
-  }
-  
-  async componentDidMount() {
-    const res = await fetch(
-      "https://api-concesionario-taller6.herokuapp.com/auto"
-    );
-    const data = await res.json();
-    this.setState({ objeto: data });
-    console.log(this.state.objeto);
-  }
-  funcionBorrar = async (event) => {
+    const res = await fetch("https://api-concesionario-taller6.herokuapp.com/auto");
+   const data = await res.json();
+   setObjeto(data);
+   console.log(objeto);
+   
+  }, [])
+  const funcionBorrar = async (event) => {
     event.preventDefault();
     console.log(" se hizo click para borara el coso :", event.target.value);
 
@@ -51,84 +48,142 @@ export default class Auto extends Component {
       console.log(" hubo un error :( :", error);
     }
   };
-  editarAutomovil = (event) => {
+  const editarAutomovil = (event) => {
     event.preventDefault();
     console.log(event.target);
   };
 
-  render() {
+  
     return (
       <>
-      <h2 style={{ height: 25, width: '100%' }}>Listado de Automoviles</h2>
-       <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell>clave</TableCell>
-            <TableCell align="right">anio</TableCell>
-            <TableCell align="right">Modelo</TableCell>
-            <TableCell align="right">Color</TableCell>
-            <TableCell align="right">Precio</TableCell>
-            <TableCell align="right">Vendedor</TableCell>
-            <TableCell align="right"></TableCell>
-            <TableCell align="right"></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        {this.state.objeto.autos?this.state.objeto.autos.map((auto)=> (
-            <TableRow
-              key={auto.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-              {auto.id}
-              </TableCell>
-              <TableCell align="right">{auto.year}</TableCell>
-              <TableCell align="right">{auto.name}</TableCell>
-              <TableCell align="right">{auto.color}</TableCell>
-              <TableCell align="right">{auto.price}</TableCell>
-              <TableCell align="right">{auto.user_id}</TableCell>
-              <TableCell align="right">
-              <Router>
+        <h2 style={{ height: 25, width: "100%" }}>Listado de Automoviles</h2>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+            <TableHead>
+              <TableRow>
+                <TableCell>clave</TableCell>
+                <TableCell align="right">anio</TableCell>
+                <TableCell align="right">Modelo</TableCell>
+                <TableCell align="right">Color</TableCell>
+                <TableCell align="right">Precio</TableCell>
+                <TableCell align="right">Vendedor</TableCell>
+                <TableCell align="right"></TableCell>
+                <TableCell align="right"></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {objeto.autos ? (
+                objeto.autos.map((auto) => (
+                  <TableRow
+                    key={auto.id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {auto.id}
+                    </TableCell>
+                    <TableCell align="right">{auto.year}</TableCell>
+                    <TableCell align="right">{auto.name}</TableCell>
+                    <TableCell align="right">{auto.color}</TableCell>
+                    <TableCell align="right">{auto.price}</TableCell>
+                    <TableCell align="right">{auto.user_id}</TableCell>
+                    <TableCell align="right">
+                      <form onSubmit={handleSubmitModificar}>
+                        <input
+                          type="text"
+                          hidden
+                          name="idInputModif"
+                          value={auto.id}
+                          onChange={(e) => {
+                            console.log(
+                              "se hizo clicl  en modificar perro",
+                              e.target.idInputModif
+                            );
+                          }}
+                        />
+                        
+                        <input
+                          type="text"
+                          hidden
+                          name="yearInputModif"
+                          value={auto.year}
+                          onChange={(e) => {}}
+                        />
+                        <input
+                          type="text"
+                          hidden
+                          name="nameInputModif"
+                          value={auto.name}
+                          onChange={(e) => {}}
+                        />
+                        <input
+                          type="text"
+                          hidden
+                          name="colorInputModif"
+                          value={auto.color}
+                          onChange={(e) => {}}
+                        />
+                        <input
+                          type="text"
+                          hidden
+                          name="precioInputModif"
+                          value={auto.price}
+                          onChange={(e) => {}}
+                        />
+                        <input
+                          type="text"
+                          hidden
+                          name="userIdInputModif"
+                          value={auto.user_id}
+                          onChange={(e) => {}}
+                        />
+                        <button
+                          type="submit"
+                          name="tipoComponente"
+                          value={"editar automovil"}
+                          className="submit-button"
+                        >
+                          Modificar
+                        </button>
+                      </form>
+                      {/* <Router>
                         <Link
                           className="submit-button"
                           to={{
                             pathname: `EditarAutomovil/${auto.id}`,
 
                             state: {
-                              detail: auto,
-                              id: auto.id,
-                              year: auto.year,
-                              name: auto.name,
-                              color: auto.color,
-                              price: auto.price,
-                              user_id: auto.user_id,
+                              
                             },
                           }}
                           value={auto.id}
                         >
                           Modificar
-                        </Link>
-                      </Router>
-              </TableCell>
-              <TableCell align="right">
-              <form>
+                        </Link> 
+                      </Router>*/}
+                    </TableCell>
+                    <TableCell align="right">
+                      <form>
                         <button
                           type="submit"
-                          onClick={this.funcionBorrar}
+                          onClick={funcionBorrar}
                           value={auto.id}
                           className="submit-button"
                         >
                           Eliminar
                         </button>
                       </form>
-              </TableCell>
-            </TableRow>
-          )):<Loading/> }
-        </TableBody>
-      </Table>
-    </TableContainer>
+                    </TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <Loading />
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </>
     );
   }
-}
+
+
+export default Auto
--- src/context/BarraNavegacionContexto.js
+++ src/context/BarraNavegacionContexto.js
@@ -12,6 +12,15 @@ const SeleccionProvider = ({children}) => {
       precio:null,
       stock:null,
       })
+      const [autoModificar, setAutoModificar] = useState({
+       
+        id: null,
+        year: null,
+        name: null,
+        color:null,
+        price: null,
+        user_id: null
+      })
     const handleSeleccion=(e)=>{
         setSeleccion(e.target.value)
        
@@ -22,24 +31,41 @@ const SeleccionProvider = ({children}) => {
 
     const handleSubmitModificar=(e)=>{
         e.preventDefault()
-        setForm({
-          ...form,
-          id:e.target.idInputModif.value,
-          tipo:e.target.tipoInputModif.value,
-          marca:e.target.marcaInputModif.value,
-          modelo:e.target.modedloInputModif.value,
-          precio:e.target.precioInputModif.value,
-          stock:e.target.stockInputModif.value,
-        })
+        if(e.target.tipoComponente.value==="editar automovil"){
+          setAutoModificar({
+           
+            id: e.target.idInputModif.value,
+            year: e.target.yearInputModif.value,
+            name: e.target.nameInputModif.value,
+            color:e.target.colorInputModif.value,
+            price:e.target.precioInputModif.value,
+            user_id: e.target.userIdInputModif.value
+            
+          })
+          setSeleccion(e.target.tipoComponente.value)
 
-        setSeleccion(e.target.tipoComponente.value)
+        }else if(e.target.tipoComponente.value==="editar repuesto"){
+          setForm({
+            
+            id:e.target.idInputModif.value,
+            tipo:e.target.tipoInputModif.value,
+            marca:e.target.marcaInputModif.value,
+            modelo:e.target.modedloInputModif.value,
+            precio:e.target.precioInputModif.value,
+            stock:e.target.stockInputModif.value,
+          })
+          setSeleccion(e.target.tipoComponente.value)
+          console.log("se hizo click en modificar", form);
+
+        }
+                              
+
         console.log("se hizo clicl  en modificar perro",e.target);
         
-        console.log("se hizo click en modificar", form);
         console.log("se Evento", e.target);
       }
 
-    const data={setSeleccion,seleccion,handleSeleccion,handleSubmitModificar,form}
+    const data={autoModificar,setSeleccion,seleccion,handleSeleccion,handleSubmitModificar,form}
     return <BarraNavegacionContexto.Provider value={data}>{children}</BarraNavegacionContexto.Provider>
     
 }
