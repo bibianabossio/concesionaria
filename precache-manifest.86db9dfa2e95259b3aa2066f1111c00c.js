48c7aba6c4f85d193c2679d7352d48a89d3005c9
cataldoivan <cmiarg@gmail.com>
2021-10-24 01:43:27 -0300

    Updates

---
+++ precache-manifest.86db9dfa2e95259b3aa2066f1111c00c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c40a855fdba51242ea0ecfe75e56c7",
+    "url": "/concesionaria/index.html"
+  },
+  {
+    "revision": "3832a5b26b1d66caffb5",
+    "url": "/concesionaria/static/css/main.6253b894.chunk.css"
+  },
+  {
+    "revision": "5f7db59dd7b2809112bd",
+    "url": "/concesionaria/static/js/2.22fda88b.chunk.js"
+  },
+  {
+    "revision": "fbec3ef3c7cc3ed2e44fea7fcdd95b5e",
+    "url": "/concesionaria/static/js/2.22fda88b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a08927d61b671ed017e4",
+    "url": "/concesionaria/static/js/3.6c03d69c.chunk.js"
+  },
+  {
+    "revision": "3832a5b26b1d66caffb5",
+    "url": "/concesionaria/static/js/main.17301298.chunk.js"
+  },
+  {
+    "revision": "1b2e95b5d1525474879a",
+    "url": "/concesionaria/static/js/runtime-main.f598242e.js"
+  }
+]);
