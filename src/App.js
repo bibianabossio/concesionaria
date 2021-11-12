4daef92fade5730277049050d14db1b827dc73c6
Nespresso <cmiarg@gmail.com>
2021-11-12 17:23:39 -0300

    commit de prueba

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
     console.log("esta expirado???", isExpired);
 
   } 
+  /* sdsds*/
   /* useEffect(() => {
     if (localStorage.getItem("activo")=== "true"   ) {
       if(!isExpired){
